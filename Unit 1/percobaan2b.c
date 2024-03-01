2929dd58db24cc9825553026123e095bb338d824
ryzakka <161613798+ryzakka@users.noreply.github.com>
2024-03-01 12:48:34 +0700

    Update percobaan2b.c

--- Unit 1/percobaan2b.c
+++ Unit 1/percobaan2b.c
@@ -4,3 +4,5 @@ int main() {
   printf(Alpro itu mudah!);
   return 0;
 }
+
+//dalam program ini, isi dari perintah "printf" yang berupa karakter tidak diapit tanda petik dua. sehingga program tidak berjalan.
