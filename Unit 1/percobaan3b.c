46b962e2d73d6b0c93000ec5227076d8530dc5a5
ryzakka <161613798+ryzakka@users.noreply.github.com>
2024-03-02 15:38:40 +0700

    Create percobaan3b.c

---
+++ Unit 1/percobaan3b.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  printf("Oh my, oh my God \n예상했어 나 \nI was really hoping that he will come through");
+  return 0;
+}
+
+//keluaran yang dihasilkan dari perintah "\n" adalah membuat baris baru.
+//sehingga pesan yang terletak setelah perintah akan menjadi baris baru dan tidak diteruskan dalam baris yang sama.
